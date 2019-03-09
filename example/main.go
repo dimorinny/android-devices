b0fd6a4de34b8172569409a281cde3dbc74bd76c
damerkurev <dmerkurev@avito.ru>
2019-03-09 03:53:25 +0300

    add example

---
+++ example/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/davecgh/go-spew/spew"
+	"github.com/dimorinny/android-devices"
+	"log"
+)
+
+func main() {
+	devices, err := android.Devices()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	spew.Dump(devices)
+}
