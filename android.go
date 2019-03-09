5d8208e93c7c495be9ea9863d2ea0ade6ccf7208
damerkurev <dmerkurev@avito.ru>
2019-03-09 03:24:45 +0300

    base structure

--- android.go
+++ android.go
@@ -1,1 +1,57 @@
 package android
+
+import (
+	"errors"
+	"github.com/google/gousb"
+	"log"
+)
+
+func Devices() ([]*Device, error) {
+	context := gousb.NewContext()
+	defer func() {
+		err := context.Close()
+		if err != nil {
+			log.Println("Android devices detector: failed to close context", err)
+		}
+	}()
+
+	devices, err := context.OpenDevices(isAndroidDevice)
+
+	defer func() {
+		for _, device := range devices {
+			_ := device.Close()
+		}
+	}()
+
+	if err != nil {
+		return nil, err
+	}
+
+	var result []*Device
+	for _, device := range devices {
+		result = append(
+			result,
+			mapLibUsbDevicesToInternalModel(device.Desc),
+		)
+	}
+
+	return nil, errors.New("error")
+}
+
+// TODO detect android device
+func isAndroidDevice(description *gousb.DeviceDesc) bool {
+	return true
+}
+
+// TODO: fetch description
+func mapLibUsbDevicesToInternalModel(description *gousb.DeviceDesc) *Device {
+	return &Device{
+		Description: "",
+
+		Bus:     description.Bus,
+		Address: description.Address,
+
+		Vendor:  int(description.Vendor),
+		Product: int(description.Product),
+	}
+}
